59e3cc62645ae898d4a019d6a1eb256ba028cfe5
Markus Weigelt <markus.weigelt@slub-dresden.de>
2024-09-26 11:08:29 +0200

    Remove generated .env file of tests

--- Classes/Validation/ApplicationProfileValidationStack.php
+++ Classes/Validation/ApplicationProfileValidationStack.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace Slub\Dfgviewer\Validation;
 
-use Kitodo\Dlf\Validation\BaseValidationStack;
+use Kitodo\Dlf\Validation\AbstractDlfValidationStack;
 
-class ApplicationProfileValidationStack extends BaseValidationStack {
+class ApplicationProfileValidationStack extends AbstractDlfValidationStack {
 
     public function __construct(array $options = [])
     {
         parent::__construct(\DOMDocument::class, $options);
 
-        $this->addValidationItem(LogicalStructureValidator::class, "Specifications for the logical document structure", false);
-        $this->addValidationItem(PhysicalStructureValidator::class, "Specifications for the logical document structure", false);
+        $this->addValidator(LogicalStructureValidator::class, "Specifications for the logical document structure", false);
+        $this->addValidator(PhysicalStructureValidator::class, "Specifications for the logical document structure", false);
 
     }
 
